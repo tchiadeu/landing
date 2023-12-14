c248d96a416dbbe8ead77843478030432455ebac
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-12-14 11:09:46 +0100

    done

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,34 +1,68 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "navbar", "button" ]
+  static targets = [ "navbar", "button", "section", "input" ]
 
-  changeColors(event) {
-    console.log(this.navbarTarget)
-    const currentColor = event.currentTarget.getAttribute('data-color')
-    if (currentColor === 'white') {
-      this.buttonTargets.forEach((button => {
-        button.classList.remove('bg-gray-900')
-        button.classList.add('bg-white')
-        if (button === event.currentTarget) {
-          button.classList.add('text-black')
-          button.classList.add('border-gray-900')
-          button.classList.add('border-black')
-          button.classList.remove('text-gray-900')
-          button.classList.remove('hover:bg-gray-700')
-          button.classList.remove('hover:bg-gray-300')
-          button.classList.remove('hover:text-white')
-          button.classList.remove('hover:text-gray-900')
+  changeSection(event) {
+    const section = event.currentTarget.getAttribute("data-section")
+    this.sectionTargets.forEach((element) => {
+      if (section === element.getAttribute("data-section")) {
+        element.classList.remove("hidden")
+      } else {
+        element.classList.add("hidden")
+      }
+    })
+    if (section === "agency" || section === "references") {
+      this.navbarTarget.classList.remove("border-gray-900")
+      this.navbarTarget.classList.add("border-white")
+      this.buttonTargets.forEach((element) => {
+        if (element === event.currentTarget) {
+          element.classList.remove("border-white")
+          element.classList.remove("text-white")
+          element.classList.remove("text-gray-300")
+          element.classList.remove("text-gray-800")
+          element.classList.add("border-2")
+          element.classList.add("border-gray-900")
+          element.classList.add("text-gray-900")
         } else {
-          button.classList.remove('border-gray-900')
-          button.classList.add('border-white')
-          button.classList.add('hover:bg-gray-300')
-          button.classList.add('hover:text-gray-900')
+          element.classList.remove("border-2")
+          element.classList.remove("text-white")
+          element.classList.remove("text-gray-300")
+          element.classList.remove("text-gray-900")
+          element.classList.add("text-gray-800")
         }
-      }))
-      this.navbarTarget.classList.remove('border-gray-900')
-      this.navbarTarget.classList.add('border-white')
+      })
+    } else if (section === "talent") {
+      this.navbarTarget.classList.remove("border-white")
+      this.navbarTarget.classList.add("border-gray-900")
+      this.buttonTargets.forEach((element) => {
+        if (element === event.currentTarget) {
+          element.classList.remove("border-gray-900")
+          element.classList.remove("text-gray-800")
+          element.classList.remove("text-gray-300")
+          element.classList.remove("text-gray-900")
+          element.classList.add("border-2")
+          element.classList.add("border-white")
+          element.classList.add("text-white")
+        } else {
+          element.classList.remove("border-2")
+          element.classList.remove("text-white")
+          element.classList.remove("text-gray-800")
+          element.classList.remove("text-gray-900")
+          element.classList.add("text-gray-300")
+        }
+      })
     } else {
+      this.navbarTarget.classList.remove("border-white")
+      this.navbarTarget.classList.add("border-gray-900")
+      this.buttonTargets.forEach((element) => {
+        element.classList.remove("border-2")
+        element.classList.remove("text-white")
+        element.classList.remove("text-gray-800")
+        element.classList.remove("text-gray-900")
+        element.classList.add("text-gray-300")
+      })
+      this.inputTarget.focus()
     }
   }
 }
