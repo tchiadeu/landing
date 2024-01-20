5246a1812c2afabd4f20ce1da2361f38e8b76579
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-01-20 14:12:38 +0100

    routes for talents & references

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'pages#home'
   get 'contact.vcf', to: 'pages#contact', as: 'contact'
+
   get 'dashboard', to: 'pages#dashboard'
+
+  resources :talents, only: %i[new create edit update destroy]
+  resources :references, only: %i[new create edit update destroy]
 end
