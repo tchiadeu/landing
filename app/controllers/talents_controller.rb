4c09680dea46632faa7ad3e53cef7bac97b9db70
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-01-20 14:15:54 +0100

    method new & create & talent_params for talents

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -1,2 +1,20 @@
 class TalentsController < ApplicationController
+  def new
+    @talent = Talent.new
+  end
+
+  def create
+    @talent = Talent.new(talent_params)
+    if @talent.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def talent_params
+    params.require(:talent).permit(:first_name, :last_name, :job, :scoail_network, :avatar)
+  end
 end
