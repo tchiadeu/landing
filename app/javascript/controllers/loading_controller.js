5732340b91eafcfd447699b5bdcf4090ef2d6052
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-02-06 09:45:47 +0100

    fix appear loading

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -2,18 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading"
 export default class extends Controller {
-  static targets = [ "loading", "navbar", "hero" ]
+  static targets = [ "loading", "container", "hero" ]
   connect() {
-    let opacity = 0
     setTimeout(() => {
       this.loadingTarget.remove()
-      this.heroTarget.classList.remove('hidden')
-      this.navbarTarget.classList.remove('hidden')
-      setInterval(() => {
-        opacity += 0.1
-        this.navbarTarget.style.opacity = opacity
-        this.heroTarget.style.opacity = opacity
-      }, 50)
-    }, 6500)
+      this.containerTarget.classList.remove('relative w-screen h-screen displayed')
+      this.heroTarget.classList.remove('absolute')
+    }, 6700)
   }
 }
