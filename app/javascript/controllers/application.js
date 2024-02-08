53db6c54a6d0284d7efc84cf87b2ba186552f301
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-02-08 16:13:31 +0100

    try something

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+// import Glow from 'stimulus-glow'
 
 const application = Application.start()
+// application.register('glow', Glow)
 
 // Configure Stimulus development experience
 application.debug = false
