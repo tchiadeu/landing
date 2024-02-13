f21446518f46a3a4ef170d75378efa06dffd1672
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-02-13 16:14:18 +0100

    try things, not working cuz of css

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
+import Lightbox from "stimulus-lightbox"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+Stimulus.register("lightbox", Lightbox)
 
 export { application }
