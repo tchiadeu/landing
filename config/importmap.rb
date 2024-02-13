860f4d97352a508ba8a85480d01549114b64b546
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-02-13 14:05:23 +0100

    remove not necessary js in importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,4 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "stimulus-glow", to: "https://cdn.jsdelivr.net/npm/stimulus-glow@0.3.0/dist/stimulus-glow.umd.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
