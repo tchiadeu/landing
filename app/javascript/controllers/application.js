f27811346a44d9817fb6d6af7fe31595c4e1fd63
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-02-13 13:10:25 +0100

    remove not necessaries comments

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-// import Glow from 'stimulus-glow'
 
 const application = Application.start()
-// application.register('glow', Glow)
 
 // Configure Stimulus development experience
 application.debug = false
