4145536d0eab9847a61cec1176800f6ddfb62a32
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-03-22 14:32:28 +0100

    done for informations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   root 'pages#home'
   post '/send_contact', to: 'pages#send_contact'
