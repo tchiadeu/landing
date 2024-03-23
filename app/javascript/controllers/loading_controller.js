5fe74c4b09b2410e781f7d1c4a53c0e4ac2b1afe
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-03-23 10:52:07 +0100

    automatic redirection from root_path to agency_path

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -2,16 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading"
 export default class extends Controller {
-  static targets = [ "loading", "container", "hero", "talent" ]
   connect() {
-    // this.resizeSection()
-    setTimeout(() => {
-      this.loadingTarget.remove()
-      this.containerTarget.classList.remove('relative')
-      this.containerTarget.classList.remove('w-screen')
-      this.containerTarget.classList.remove('h-screen')
-      this.containerTarget.classList.remove('displayed')
-      this.heroTarget.classList.remove('absolute')
-    }, 10000)
+    const currentUrl = window.location.href
+    let interval = setInterval(() => {
+      if (parseFloat(window.getComputedStyle(this.element).opacity) === 0) {
+        window.location.href = `${currentUrl}agency`
+        clearInterval(interval)
+      }
+    }, 50)
   }
 }
